58a30355402cb84e1e55b84161ac057d2bf583ba
Edward-OA <okoloedward19@gmail.com>
2025-11-03 22:43:20 +0100

    New

--- the-wild-oasis/src/features/cabins/AddCabin.jsx
+++ the-wild-oasis/src/features/cabins/AddCabin.jsx
@@ -1,32 +1,25 @@
-import { useState } from 'react';
-import Button from '../../ui/Button';
-import Modal from '../../ui/Modal';
-import CreateCabinForm from './CreateCabinForm';
-import CabinTable from './CabinTable';
+import Button from "../../ui/Button";
+import Modal from "../../ui/Modal";
+import CreateCabinForm from "./CreateCabinForm";
 
 function AddCabin() {
   return (
-    <Modal>
-      <Modal.Open opens="cabin-form">
-        <Button>Add new cabin</Button>
-      </Modal.Open>
+    <div>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
         <Modal.Window name="cabin-form">
           <CreateCabinForm />
         </Modal.Window>
-
-      <Modal.Open opens="table">
-        <Button>Show table</Button>
-      </Modal.Open>
-        <Modal.Window name="table">
-          <CabinTable />
-        </Modal.Window>
-    </Modal>
+      </Modal>
+    </div>
   );
 }
 // function AddCabin() {
 //   const [isOpenModal, setIsOpenModal] = useState(false);
 
-//   return ( 
+//   return (
 //     <div>
 //       <Button onClick={() => setIsOpenModal((show) => !show)}>
 //         Add new Cabin
