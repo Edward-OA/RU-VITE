d4213a9c52525a84300c268303385daa08ed081d
Edward-OA <okoloedward19@gmail.com>
2025-11-04 07:33:11 +0100

    Add sample data and components for cabin deletion feature
    Introduces a new directory with sample data, images, and React components related to confirming cabin deletions. Also includes supporting UI elements, authentication forms, and hooks. Updates to existing CabinRow, useOutsideClick, and ConfirmDelete components are present to support the new feature.

---
+++ the-wild-oasis/src/370  Confirming Cabin Deletions/App.jsx
@@ -0,0 +1,114 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
+import Dashboard from './pages/Dashboard';
+import Account from './pages/Account';
+import Cabins from './pages/Cabins';
+import Bookings from './pages/Bookings';
+import Settings from './pages/Settings';
+import Login from './pages/Login';
+import NewUsers from './pages/Users';
+import PageNotFound from './pages/PageNotFound';
+import GlobalStyles from './styles/GlobalStyles';
+import AppLayout from './ui/AppLayout';
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 0 } },
+});
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <GlobalStyles />
+      <BrowserRouter
+        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+      >
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="account" element={<Account />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="users" element={<NewUsers />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: '8px' }}
+        toastOptions={{
+          success: { duration: 3000 },
+          error: { duration: 5000 },
+          style: {
+            fontSize: '16px',
+            maxWidth: '500px',
+            padding: '16px 24px',
+            backgroundColor: 'var(--color-grey-0)',
+            color: 'var(--color-grey-700)',
+          },
+        }}
+      />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
+
+// import styled from 'styled-components';
+// import GlobalStyles from './styles/GlobalStyles';
+// import Button from './ui/Button';
+// import Input from './ui/Input';
+// import Heading from './ui/Heading';
+// import Row from './ui/Row';
+
+// const StyledApp = styled.div`
+//   /* background-color: orangered; */
+//   padding: 20px;
+// `;
+
+// function App() {
+//   return (
+//     <>
+//       <GlobalStyles />
+//       <StyledApp>
+//         <Row>
+//           <Row type="horizontal">
+//             <Heading type="h1">The Wild Oasis</Heading>
+//             <div>
+//               <Heading as="h2">Checkin n Checkout</Heading>
+//               <Button
+
+//                 onClick={() => alert('Check in')}
+//               >
+//                 Check in
+//               </Button>
+//               <Button
+//                 variation="secondary"
+//                 size="small"
+//                 onClick={() => alert('Check out')}
+//               >
+//                 Check out
+//               </Button>
+//             </div>
+//           </Row>
+//           <Row type="vertical">
+//             <Heading as="h3">Form</Heading>
+//             <form>
+//               <Input type="number" placeholder="Number of guests" />
+//               <Input type="number" placeholder="Number of guests" />
+//             </form>
+//           </Row>
+//         </Row>
+//       </StyledApp>
+//     </>
+//   );
+// }
+
+// export default App;
