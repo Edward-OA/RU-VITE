945a6ca77bf8da234c7d21a34bdc5869e1afd96f
Edward-OA <okoloedward19@gmail.com>
2025-11-09 23:42:21 +0100

    Add contextual menu to CabinRow and implement Menus UI
    Introduces a new Menus component with context, toggle, list, and button functionality for displaying contextual actions. Integrates the menu into CabinRow, providing Duplicate, Edit, and Delete options for each cabin.

--- the-wild-oasis/src/ui/Menus.jsx
+++ the-wild-oasis/src/ui/Menus.jsx
@@ -1,6 +1,9 @@
+import { createContext, useContext, useState } from "react";
+import { createPortal } from "react-dom";
+import { HiEllipsisVertical } from "react-icons/hi2";
 import styled from "styled-components";
 
-const StyledMenu = styled.div`
+const Menu = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
@@ -61,8 +64,64 @@ const StyledButton = styled.button`
   }
 `;
 
-function Menus() {
-  return <div>{children}</div>;
+const MenusContext = createContext();
+
+function Menus({ children }) {
+  const [openId, setOpenId] = useState("");
+  const [position, setPosition] = useState(null);
+  const close = () => setOpenId("");
+  const open = setOpenId;
+  return (
+    <MenusContext.Provider
+      value={{ openId, close, open, position, setPosition }}
+    >
+      {children}
+    </MenusContext.Provider>
+  );
 }
+
+function Toggle({ id }) {
+  const { openId, open, close, setPosition } = useContext(MenusContext);
+
+  function handleClick(e) {
+    const rect = e.target.closest("button").getBoundingClientRect();
+
+    setPosition({
+      x: window.innerWidth - rect.width - rect.x,
+      y: rect.y + rect.height + 8,
+    });
+    // console.log(rect);
+
+    openId === "" || openId !== id ? open(id) : close();
+  }
+  return (
+    <StyledToggle onClick={handleClick}>
+      <HiEllipsisVertical />
+    </StyledToggle>
+  );
+}
+
+function List({ id, children }) {
+  const { openId, position } = useContext(MenusContext);
+
+  if (openId !== id) return null;
+  return createPortal(
+    <StyledList position={position}>{children}</StyledList>,
+    document.body
+  );
+}
+
+function Button({ children }) {
+  return (
+    <li>
+      <StyledButton>{children}</StyledButton>
+    </li>
+  );
+}
+
+Menus.Menu = Menu;
+Menus.Toggle = Toggle;
+Menus.List = List;
+Menus.Button = Button;
 
 export default Menus;
